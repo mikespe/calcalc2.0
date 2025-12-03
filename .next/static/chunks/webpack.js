2ac943cb7a4dbf5adbd003148a99b66243f26714
mikespe <mikesperanza34@gmail.com>
2025-12-02 21:46:03 -0500

    package update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10813d0dc06cd39b"; }
+/******/ 		__webpack_require__.h = function() { return "cbcddf6389adf13d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
